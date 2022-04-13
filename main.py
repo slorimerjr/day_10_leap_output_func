ecb0cefff624feaa5c0fcdc8658f8fbbd40c3da1
Sam Lorimer <slorimerjr@gmail.com>
2022-04-13 17:01:53 +0000

    added teacher's solution

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#my solution
 def is_leap(year):
   if year % 4 == 0:
     if year % 100 == 0:
@@ -17,21 +18,37 @@ def days_in_month(year, month):
       month_days[1] = 29
   return month_days[month]
   
-    
-
-  
-  
-#🚨 Do NOT change any of the code below 
+# #🚨 Do NOT change any of the code below 
 year = int(input("Enter a year: "))
 month = int(input("Enter a month: "))
 days = days_in_month(year, month)
 print(days)
 
 
+#teacher solution
+def is_leap(year):
+  if year % 4 == 0:
+    if year % 100 == 0:
+      if year % 400 == 0:
+        return True
+      else:
+        return False
+    else:
+     return True
+  else:
+    return False
 
-
-# year = int(input("Enter a year: "))
-# print(is_leap(year))
+def days_in_month(year, month):
+  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  if is_leap(year) and month == 2:
+    return 29
+  return month_days[month - 1]
+  
+#🚨 Do NOT change any of the code below 
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days = days_in_month(year, month)
+print(days)
 
 
 
